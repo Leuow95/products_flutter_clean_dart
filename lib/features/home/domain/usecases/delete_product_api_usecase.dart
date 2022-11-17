8082d2e0b8260e7b4937962471c82cebefc8f7d4
leonardomaia <leuow95@gmail.com>
2022-11-17 16:49:21 -0400

    feat: implemented deleteProduct usecase

---
+++ lib/features/home/domain/usecases/delete_product_api_usecase.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:products_challenge/features/home/domain/errors/product_failure.dart';
+import 'package:products_challenge/features/home/domain/repositories/product_repository.dart';
+
+abstract class DeleteProductUsecase {
+  Future<Either<ProductFailure, bool>> deleteProductByIndex(int index);
+}
+
+class DeleteProductsUsecaseImpl implements DeleteProductUsecase {
+  final ProductRepository repository;
+
+  DeleteProductsUsecaseImpl(this.repository);
+
+  @override
+  Future<Either<ProductFailure, bool>> deleteProductByIndex(int index) async {
+    final eitherResponse = await repository.deleteProduct(index);
+
+    return eitherResponse.fold(
+      (failure) => left(failure),
+      (sucess) => right(true),
+    );
+  }
+}
