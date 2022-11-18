28a1b3c27fc0f83e0730be5d4e61fdf33b465ff6
leonardomaia <leuow95@gmail.com>
2022-11-18 16:45:56 -0400

    feat: disabled the old firebase datasource

--- lib/features/home/external/datasource/firebase_datasource.dart
+++ lib/features/home/external/datasource/firebase_datasource.dart
@@ -1,41 +1,41 @@
-import 'package:firebase_database/firebase_database.dart';
-import 'package:products_challenge/features/home/infra/datasources/products_datasource.dart';
-import 'package:products_challenge/features/home/infra/models/product_model.dart';
+// import 'package:firebase_database/firebase_database.dart';
+// import 'package:products_challenge/features/home/infra/datasources/products_datasource.dart';
+// import 'package:products_challenge/features/home/infra/models/product_model.dart';
 
-class FirebaseDataSource implements ProductsDataSource {
-  final DatabaseReference database;
+// class FirebaseDataSource implements ProductsDataSource {
+//   final DatabaseReference database;
 
-  FirebaseDataSource(this.database);
+//   FirebaseDataSource(this.database);
 
-  @override
-  Future<List<ProductModel>> getProducts() async {
-    try {
-      final List<ProductModel> list = [];
-      final DataSnapshot dataSnapshot = await database.get();
+//   // @override
+//   // Future<List<ProductModel>> getProducts() async {
+//   //   try {
+//   //     final List<ProductModel> list = [];
+//   //     final DataSnapshot dataSnapshot = await database.get();
 
-      final jsonObject = List?.from(dataSnapshot.value as List);
+//   //     final jsonObject = List?.from(dataSnapshot.value as List);
 
-      // ignore: avoid_function_literals_in_foreach_calls
-      jsonObject.forEach((element) {
-        if (element != null) {
-          list.add(ProductModel.fromMap(element));
-        }
-      });
+//   //     // ignore: avoid_function_literals_in_foreach_calls
+//   //     jsonObject.forEach((element) {
+//   //       if (element != null) {
+//   //         list.add(ProductModel.fromMap(element));
+//   //       }
+//   //     });
 
-      return list;
-    } catch (e) {
-      return [];
-    }
-  }
+//   //     return list;
+//   //   } catch (e) {
+//   //     return [];
+//   //   }
+//   // }
 
-  @override
-  Future<bool> deleteProduct(int index) async {
-    try {
-      final ref = FirebaseDatabase.instance.ref();
-      ref.child("/$index").remove();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-}
+//   @override
+//   Future<bool> deleteProduct({required String id}) async {
+//     try {
+//       final ref = FirebaseDatabase.instance.ref();
+//       ref.child("/$id").remove();
+//       return true;
+//     } catch (e) {
+//       return false;
+//     }
+//   }
+// }
