d934b4f77cfc6cc62a3ef176a18bb508346f5b2d
leonardomaia <leuow95@gmail.com>
2022-11-17 23:27:09 -0400

    feat: add delete to controller

--- lib/features/home/presenter/controllers/home_controller.dart
+++ lib/features/home/presenter/controllers/home_controller.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:products_challenge/features/home/domain/usecases/delete_product_api_usecase.dart';
 import 'package:products_challenge/features/home/domain/usecases/get_products_api_usecase.dart';
 import 'package:products_challenge/features/home/presenter/controllers/state/products_state.dart';
 
 class HomeController extends ValueNotifier<ProductState> {
   final GetProductApiUseCase getProductsUsecase;
+  final DeleteProductUsecase deleteProductUsecase;
 
-  HomeController({required this.getProductsUsecase})
-      : super(ProductInitialState());
+  HomeController({
+    required this.getProductsUsecase,
+    required this.deleteProductUsecase,
+  }) : super(ProductInitialState());
 
   Future<void> fetchProducts() async {
     value = ProductLoadingState();
 
-    final products = await getProductsUsecase.call();
+    var products = await getProductsUsecase.call();
 
     products.fold(
       (failure) => value = ProductFailureState(value.toString()),
       (products) => value = ProductSuccessState(products),
+    );
+  }
+
+  Future<void> deleteProductByIndex(int index) async {
+    value = ProductLoadingState();
+
+    final eitherResponse = await deleteProductUsecase.call(index);
+
+    eitherResponse.fold(
+      (failure) => value = ProductFailureState(failure.toString()),
+      (success) => fetchProducts(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-//importas as dependencias quando trabalhar no presenter
 import 'package:firebase_core/firebase_core.dart';
-import 'package:products_challenge/features/home/presenter/controllers/pages/home_page.dart';
+import 'features/home/presenter/pages/home_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
