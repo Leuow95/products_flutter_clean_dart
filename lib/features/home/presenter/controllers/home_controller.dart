7b1976141465de1a6b35a50ccd8e144f69a90317
leonardomaia <leuow95@gmail.com>
2022-11-19 23:51:03 -0400

    feat: implemented addproducts to home controller

--- lib/features/home/presenter/controllers/home_controller.dart
+++ lib/features/home/presenter/controllers/home_controller.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:products_challenge/features/home/domain/entities/product_entity.dart';
+import 'package:products_challenge/features/home/domain/usecases/add_product_api_usecase.dart';
 import 'package:products_challenge/features/home/domain/usecases/delete_product_api_usecase.dart';
 import 'package:products_challenge/features/home/domain/usecases/get_products_api_usecase.dart';
 import 'package:products_challenge/features/home/presenter/controllers/state/products_state.dart';
+import 'package:products_challenge/features/home/presenter/params/add_products_params.dart';
 
 class HomeController extends ValueNotifier<ProductState> {
   final GetProductApiUseCase getProductsUsecase;
   final DeleteProductUsecase deleteProductUsecase;
+  final AddProductUsecase addProductUsecase;
 
   HomeController({
     required this.getProductsUsecase,
     required this.deleteProductUsecase,
+    required this.addProductUsecase,
   }) : super(ProductInitialState());
 
   Future<void> fetchProducts() async {
@@ -31,6 +36,30 @@ class HomeController extends ValueNotifier<ProductState> {
     eitherResponse.fold(
       (failure) => value = ProductFailureState(failure.toString()),
       (success) => fetchProducts(),
+    );
+  }
+
+  Future<void> addProduct({
+    required AddProductsParams params,
+  }) async {
+    value = ProductLoadingState();
+
+    final eitherResponse = await addProductUsecase.call(
+      productEntity: ProductEntity(
+          id: params.id,
+          title: params.title,
+          type: params.type,
+          description: params.description,
+          filename: params.filename,
+          height: params.height,
+          width: params.width,
+          price: params.price,
+          rating: params.rating),
+    );
+
+    eitherResponse.fold(
+      (failure) => value = ProductFailureState(failure.toString()),
+      (success) => value = AddProductSuccessState(),
     );
   }
 }
