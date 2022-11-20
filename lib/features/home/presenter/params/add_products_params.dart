4a154ae73e03e28d12ab84f3d4916a189bc7ac2c
leonardomaia <leuow95@gmail.com>
2022-11-19 23:51:29 -0400

    feat: created addproducts params to controller

---
+++ lib/features/home/presenter/params/add_products_params.dart
@@ -0,0 +1,23 @@
+class AddProductsParams {
+  final String id;
+  final String title;
+  final String type;
+  final String description;
+  final String filename;
+  final int height;
+  final int width;
+  final double price;
+  final int rating;
+
+  AddProductsParams({
+    required this.id,
+    required this.title,
+    required this.type,
+    required this.description,
+    required this.filename,
+    required this.height,
+    required this.width,
+    required this.price,
+    required this.rating,
+  });
+}
