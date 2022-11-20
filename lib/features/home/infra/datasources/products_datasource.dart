472d62c4f4f6196014af5aaa0110142c49fa3215
leonardomaia <leuow95@gmail.com>
2022-11-19 23:38:55 -0400

    feat: added addproducts to products datasource

--- lib/features/home/infra/datasources/products_datasource.dart
+++ lib/features/home/infra/datasources/products_datasource.dart
@@ -3,4 +3,5 @@ import 'package:products_challenge/features/home/infra/models/product_model.dart
 abstract class ProductsDataSource {
   Future<List<ProductModel>> getProducts();
   Future<bool> deleteProduct({required String id});
+  Future<bool> addProduct({required ProductModel productModel});
 }
