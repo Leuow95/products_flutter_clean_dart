c7467b2aa07d810c952fe4566a2dea01a6d8991d
leonardomaia <leuow95@gmail.com>
2022-11-28 21:30:54 -0400

    feat: created user entity

---
+++ lib/features/login/domain/entities/user_entity.dart
@@ -0,0 +1,11 @@
+class UserEntity {
+  final String name;
+  final String email;
+  final String password;
+
+  UserEntity({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
