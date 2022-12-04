2e9290d7352822b21381ccd4bd59d99e3ad7cc42
leonardomaia <leuow95@gmail.com>
2022-12-04 13:26:28 -0400

    refactor: changed name of RegisterEntity

---
+++ lib/features/auth/register/domain/entities/register_user_entity.dart
@@ -0,0 +1,11 @@
+class RegisterUserEntity {
+  final String userName;
+  final String email;
+  final String password;
+
+  RegisterUserEntity({
+    required this.userName,
+    required this.email,
+    required this.password,
+  });
+}
