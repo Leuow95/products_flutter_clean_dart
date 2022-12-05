c9b97ff00d07fd728b5bc732996cd721c56421d6
leonardomaia <leuow95@gmail.com>
2022-12-04 23:05:02 -0400

    feat: implemented register page controller

---
+++ lib/features/auth/register/presenter/controller/register_page_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:products_challenge/features/auth/register/domain/usecases/register_usecase.dart';
+import 'package:products_challenge/features/auth/register/presenter/controller/register_page_states.dart';
+import 'package:products_challenge/features/auth/register/presenter/params/register_user_params.dart';
+
+class RegisterController extends ValueNotifier<RegisterState> {
+  final RegisterUseCase registerUseCase;
+  RegisterController(
+    RegisterState value,
+    this.registerUseCase,
+  ) : super(value);
+
+  Future<void> registerUser({
+    required RegisterUserParams params,
+  }) async {
+    value = RegisterLoadingState();
+
+    final eitherResponse = await registerUseCase.call(
+      userName: params.userName,
+      email: params.email,
+      password: params.password,
+    );
+
+    eitherResponse.fold(
+      (failure) => value = RegisterFailureState(),
+      (success) => value = RegisterSuccessState(),
+    );
+  }
+}
---
+++ lib/features/auth/register/presenter/controller/register_page_states.dart
@@ -0,0 +1,9 @@
+abstract class RegisterState {}
+
+class RegisterInitialState implements RegisterState {}
+
+class RegisterLoadingState implements RegisterState {}
+
+class RegisterSuccessState implements RegisterState {}
+
+class RegisterFailureState implements RegisterState {}
