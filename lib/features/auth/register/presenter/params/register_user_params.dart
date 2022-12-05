5cf6ffd57b4a60e450b128dc6fa1f5fdb2069d28
leonardomaia <leuow95@gmail.com>
2022-12-04 23:06:11 -0400

    feat: implemented register user params

---
+++ lib/features/auth/register/presenter/params/register_user_params.dart
@@ -0,0 +1,11 @@
+class RegisterUserParams {
+  final String userName;
+  final String email;
+  final String password;
+
+  RegisterUserParams({
+    required this.userName,
+    required this.email,
+    required this.password,
+  });
+}
